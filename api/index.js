0fba26dbfea1f897b9510e751185dcadece30a2d
cedrickomz <136609027+cedrickomz@users.noreply.github.com>
2024-06-11 22:52:40 -0400

    Add auth API route for user signup

--- api/index.js
+++ api/index.js
@@ -1,24 +1,37 @@
-import  express from 'express';
+import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 dotenv.config();
-// Connect to MongoDB
+
+// Correct MongoDB URI format
+const mongoURI = process.env.MONGO;
+
 mongoose
-.connect(process.env.MONGO)
-.then( () => {
+  .connect(mongoURI)
+  .then(() => {
     console.log('Connected to MongoDB');
-})
-.catch((error) => {
- console.log(error.message);
-});
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error.message);
+  });
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-}
-);
+  console.log('Server is running on port 3000');
+});
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+ 
