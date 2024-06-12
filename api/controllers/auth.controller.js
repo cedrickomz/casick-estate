1b4076efd381d0921e9309cd7c85ebd6975bc425
cedrickomz <136609027+cedrickomz@users.noreply.github.com>
2024-06-12 01:42:38 -0400

    Add signin API route and controller

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken';
 
 
 export const signup = async (req, res, next) => {
@@ -16,3 +17,34 @@ export const signup = async (req, res, next) => {
         next(error);
     }
 };
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+      const user = await User.findOne({ email }).exec();
+      if (!user) {
+        res.status(401).json({ message: 'Invalid email or password' });
+        return;
+      }
+  
+      const isPasswordValid = bcryptjs.compareSync(password, user.password);
+      if (!isPasswordValid) {
+        res.status(401).json({ message: 'Invalid email or password' });
+        return;
+      }
+  
+      const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
+      res.cookie('token', token, { httpOnly: true });
+  
+      const { username, email: userEmail } = user; // Destructure user information
+      res.status(200).json({
+        message: 'User logged in successfully',
+        user: { username, email: userEmail }
+      });
+    } catch (error) {
+      res.status(500).json({ message: 'An error occurred. Please try again.' });
+      next(error);
+    } finally {
+      console.log('User signed in');
+    }
+  };
--- api/index.js
+++ api/index.js
@@ -33,6 +33,4 @@ app.use('/api/auth', authRouter);
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
-}); 
-    
- 
+});
